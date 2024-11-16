3a2bd9110d9d924d6cab08a2e5436011af31028c
Marsani <marsanix@gmail.com>
2024-11-16 13:56:38 +0700

    add login page

---
+++ app/src/main/java/com/rajaalihaji/upstudy/LoginActivity2.kt
@@ -0,0 +1,152 @@
+package com.rajaalihaji.upstudy
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rajaalihaji.upstudy.ui.theme.UpSTUDYTheme
+
+class LoginActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UpSTUDYTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting3(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting3(name: String, modifier: Modifier = Modifier) {
+    LoginScreen()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview3() {
+    UpSTUDYTheme {
+        Greeting3("Android")
+    }
+}
+
+@Composable
+fun LoginScreen() {
+    val context = LocalContext.current
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            // .background(Brush.verticalGradient(colors = listOf(Color.Blue, Color.LightGray)))
+            //.padding(16.dp)
+        ,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Box(modifier = Modifier
+            .clip(RoundedCornerShape(bottomStart = 30.dp))
+            .height(250.dp)
+            .heightIn(min = 50.dp)
+            .fillMaxSize()
+            .background(Brush.verticalGradient(colors = listOf(Color.LightGray, Color.Blue)))
+
+        ) {
+            Text(modifier = Modifier.align(Alignment.Center), text = "Login", fontSize = 24.sp, color = Color.Yellow)
+        }
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(R.drawable.ice_email),
+                    contentDescription = "Email Icon"
+                )
+            },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(R.drawable.ic_lock),
+                    contentDescription = "Lock Icon"
+                )
+            },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextButton(onClick = { /* Handle forgot password */ }) {
+            Text(text = "Forgot Password?")
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+        ) {
+            TextButton(onClick = { /* Handle create account */ }) {
+                Text(text = "Create Account")
+            }
+            // Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                context.startActivity(Intent(context, MainActivity::class.java))
+            }) {
+                Text(text = "Login")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Version 1.0")
+    }
+}
--- app/src/main/java/com/rajaalihaji/upstudy/MainActivity.kt
+++ app/src/main/java/com/rajaalihaji/upstudy/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.rajaalihaji.upstudy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.rajaalihaji.upstudy.ui.theme.UpSTUDYTheme
 
@@ -17,6 +31,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             UpSTUDYTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -32,10 +47,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        TextButton(onClick = {
+            context.startActivity(Intent(context, LoginActivity2::class.java))
+        }) { Text("Login") }
+
+    }
+
 }
 
 @Preview(showBackground = true)
